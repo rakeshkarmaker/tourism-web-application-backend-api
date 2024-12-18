d621981502e6c85ff851fe0f19687233893dcb3b
rakesh <rakeshkarmaker65@gmail.com>
2024-12-18 13:03:33 +0600

    v1.6.0 - Logout added

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -14,7 +14,6 @@ import { LoginDto } from './dtos/login.dto';
 import { JwtService } from '@nestjs/jwt'; // v1.4.2- needed already given
 import { ForgotPassDto } from './dtos/forget_pass.dto';
 
-
 import { MailService } from '../mail/mail.service';
 import { sendotpDto } from './dtos/sendotp.dto';
 
@@ -28,40 +27,38 @@ export class AuthService {
     private userRepo: Repository<USER_INFO>,
 
     private jwtService: JwtService,
-    private mailService:MailService, // v1.3.1- Email authentication
-
+    private mailService: MailService, // v1.3.1- Email authentication
   ) {}
 
   //JWT Access token Generation
-  
+
   async generateUserTokens(userID: number) {
     const accessToken = this.jwtService.sign({ userID });
     //RefreashToken alternative: https://www.npmjs.com/package/uuid
     const refreshToken = this.jwtService.sign({ userID }, { expiresIn: '7d' }); // long-lived refresh token
-    
 
-    return{
+    return {
       accessToken,
-      refreshToken
+      refreshToken,
     };
-     
   }
 
   //v1.4.3- refresh Tokens for this. function declared to save token
-  async saveRefreshToken(user: LOGIN_INFO){
-    const { accessToken, refreshToken } = await this.generateUserTokens(user.id);
+  async saveRefreshToken(user: LOGIN_INFO) {
+    const { accessToken, refreshToken } = await this.generateUserTokens(
+      user.id,
+    );
 
     //Date calculation
-    const expDate =new Date();
+    const expDate = new Date();
     expDate.setDate(expDate.getDate() + 7); // Adding 7 days to the current date
 
     // Store refresh token in database
     user.refreshToken = refreshToken;
     user.refTokenExpDate = expDate;
     await this.loginRepo.save(user);
-    return {accessToken, refreshToken, expDate};
+    return { accessToken, refreshToken, expDate };
   }
-
 
   //Signup
 
@@ -118,124 +115,151 @@ export class AuthService {
 
     // return {accessToken, refreshToken, expDate};
     return await this.saveRefreshToken(user);
-    
   }
 
   //v1.3.1-Generate OTP Function
-  async generateSecureOTP(otpLength:number=8): Promise<{ otp: string; expDate: Date }|null> {//Installation: npm install uuid #Umm its overkill
+  async generateSecureOTP(
+    otpLength: number = 8,
+  ): Promise<{ otp: string; expDate: Date } | null> {
+    //Installation: npm install uuid #Umm its overkill
 
     if (otpLength <= 4) return null; // Handling the invalid length minimum 4 digit otp
 
     // Generating an 8-digit OTP
-    const otp = Array.from(crypto.getRandomValues(new Uint8Array(otpLength)), (num) => (num % 10).toString()).join('');
+    const otp = Array.from(
+      crypto.getRandomValues(new Uint8Array(otpLength)),
+      (num) => (num % 10).toString(),
+    ).join('');
 
     //Generating expiration of OTP:
     const expDate = new Date();
     expDate.setMinutes(expDate.getMinutes() + 15); // OTP is valid for 15 minutes
 
-    return {otp,expDate};
+    return { otp, expDate };
   }
 
   //v1.3.1-OTP request for, Forget Password
   async sendOtp(forgotPasswordDto: sendotpDto) {
     const { email } = forgotPasswordDto;
-  
+
     // Check if the user exists in the database
     const user = await this.loginRepo.findOne({ where: { email } });
-  
+
     if (!user) {
       throw new NotFoundException('User not found. Invalid Request.');
     }
-  
+
     // Generate a reset token
-    const {otp,expDate} =await this.generateSecureOTP();
-  
+    const { otp, expDate } = await this.generateSecureOTP();
+
     // Store the reset token and expiration date in the database
     user.resetToken = otp;
     user.resetTokenExpDate = expDate;
     await this.loginRepo.save(user);
-  
+
     // Send email with the otp => https://www.nodemailer.com/
     await this.mailService.sendMail(
       user.email,
       'Password Reset OTP Request',
-      `Use this OTP to reset your password. OTP: ${otp}`
+      `Use this OTP to reset your password. OTP: ${otp}`,
     );
-  
+
     return {
       message: 'Password reset token sent to email',
       otp, // Removable in production
     };
   }
 
-
-
   //v1.4.3- refresh Tokens
-  async refreshTokens(refreshToken:string){
+  async refreshTokens(refreshToken: string) {
     //Verifying refresh token exists and is not expired!
-    const token = await this.loginRepo.findOne({ where: { refreshToken: refreshToken } })
+    const token = await this.loginRepo.findOne({
+      where: { refreshToken: refreshToken },
+    });
 
-    if(!token || new Date(token.refTokenExpDate) <= new Date()){ //if token is invalid or time is up, throw error!
+    if (!token || new Date(token.refTokenExpDate) <= new Date()) {
+      //if token is invalid or time is up, throw error!
       throw new UnauthorizedException('Not found. Invalid Request.');
-
     }
-    return this.saveRefreshToken(token);// If the user token is valid, generating a new token
-    
+    return this.saveRefreshToken(token); // If the user token is valid, generating a new token
   }
 
   //v1.5.1- ForgetPassword
-  async generateAndHashPassword(length: number = 12): Promise<{ password: string; hash: string }> {
-    const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;:,.<>?';
-    
+  async generateAndHashPassword(
+    length: number = 12,
+  ): Promise<{ password: string; hash: string }> {
+    const charset =
+      'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;:,.<>?';
+
     // Generate a secure random password
-    const passwordArray = Array.from(crypto.getRandomValues(new Uint8Array(length)));
-    const password = passwordArray.map((num) => charset[num % charset.length]).join('');
-  
+    const passwordArray = Array.from(
+      crypto.getRandomValues(new Uint8Array(length)),
+    );
+    const password = passwordArray
+      .map((num) => charset[num % charset.length])
+      .join('');
+
     // Hash the password using bcrypt
     const hash = await bcrypt.hash(password, 11); // Cost factor for bcrypt =11 here 300ms+ time required
-  
+
     return { password, hash };
   }
 
   async forgotPass(forgotPasswordDto: ForgotPassDto) {
-    const { email,otp } = forgotPasswordDto;
-  
+    const { email, otp } = forgotPasswordDto;
+
     // Check if the user exists in the database
     const user = await this.loginRepo.findOne({ where: { email } });
-  
+
     if (!user) {
       throw new NotFoundException('User not found. Invalid Request.');
     }
 
-    if(user.resetToken===otp &&new Date(user.resetTokenExpDate) >= new Date()){ // If reset token mactchs and reset time still valid then return true => send reset Pass
+    if (
+      user.resetToken === otp &&
+      new Date(user.resetTokenExpDate) >= new Date()
+    ) {
+      // If reset token mactchs and reset time still valid then return true => send reset Pass
 
       //Generating Password
-      const {password,hash} = await this.generateAndHashPassword();
-      user.password=hash; //Setting hashed new password in the DB
+      const { password, hash } = await this.generateAndHashPassword();
+      user.password = hash; //Setting hashed new password in the DB
 
       //Sending Email to the user
-      this.mailService.sendMail(email,
-        "Your Tourify Accoount Password has been reset!",
-        `'Your Password Has been reset and Recovery pass is: ${password}`);
+      this.mailService.sendMail(
+        email,
+        'Your Tourify Accoount Password has been reset!',
+        `'Your Password Has been reset and Recovery pass is: ${password}`,
+      );
 
       //Return values
       return {
-        message: 'Your Password Has been reset and Recovery pass send to email.',
+        message:
+          'Your Password Has been reset and Recovery pass send to email.',
         New_Password: password,
       };
-
-    }else{
-
-      throw new UnauthorizedException("Invalid OTP or Email.");
+    } else {
+      throw new UnauthorizedException('Invalid OTP or Email.');
     }
-
   }
 
-  // v1.x.x - Change Passward
+  //v1.6.0 - Logout
+  // Logout
+  async logout(userId: number): Promise<{ message: string }> {
+    const user = await this.loginRepo.findOne({ where: { id: userId } });
 
+    if (!user) {
+      throw new NotFoundException('User not found.');
+    }
 
+    user.refreshToken = null;// Deleting tokens
+    user.refTokenExpDate = null;
 
-    
-  
+    await this.loginRepo.save(user);
+
+    return { message: `Logout successful.Token set to ${user.refreshToken} and date set to ${user.refTokenExpDate}` };
+  }
 
 }
+
+// v1.x.x - Change Passward
