698217fcb93a6a86f383262ddb1c171c9ccbd5d0
rakesh <rakeshkarmaker65@gmail.com>
2024-12-21 14:51:28 +0600

    v1.7.3 - Added security to Email configuration with ENV setup, ERROR [MailerService]   Bug fixed by installing letest v2.0.2 version of mailer

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -3,20 +3,22 @@ import { MailerModule } from '@nestjs-modules/mailer';
 import { Module } from '@nestjs/common';
 import { MailController } from './mail.controller';
 import { MailService } from './mail.service';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 
 
 
 @Module({
   imports: [
-    MailerModule.forRoot({//v1.3.1- adding email
+    MailerModule.forRoot({//v1.3.1- adding email | v1.7.3 - Added env security
       transport:{
-        host:'smtp.gmail.com',
-        port: 465, // Port 587 for TLS
-        secure: true, // Use TLS, not SS
+        host:process.env.MAIL_HOST,
+        port: parseInt(process.env.MAIL_PORT, 10), // Port 587 for TLS
+        secure: process.env.MAIL_SECURE === 'true', // Use TLS, not SS
         auth:{
-          user:'tourifybd@gmail.com',
-          pass:'gzpyrxagtjtawehh',
+          user:process.env.MAIL_USER,
+          pass:process.env.MAIL_PASS,
         },
         tls: {
             rejectUnauthorized: false,
