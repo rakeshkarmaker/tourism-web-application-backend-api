8129ed820fe0dbecd115da6af15375dc6e54386d
rakesh <rakeshkarmaker65@gmail.com>
2024-12-21 18:29:49 +0600

    V1.7.5- logout added to controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -12,40 +12,38 @@ import { AuthGuard } from '../guards/auth.guard';
 export class AuthController {
   constructor(private readonly AuthService:AuthService){}
 
-  //Post Signup
-  @Post('signup')
+  
+  @Post('signup')//Post Signup
   async signUp(@Body() signupData:SignupDto){
     
     return this.AuthService.signup(signupData);
 
   }
-  //Post Login
-  @Post('login')
+  
+  @Post('login')//Post Login
   async login(@Body() credentials:LoginDto){
     return this.AuthService.login(credentials)
 
 
   }
-
   
-  //Post Refeash Token
-  //v1,3.1 - Req OTP
-  @Post('reqOtp')
+  @Post('reqOtp')//v1,3.1 - Req OTP
   async reqOtp(@Body() email:sendotpDto){
     return this.AuthService.sendOtp(email);
   }
 
-  //v1.4.3- Refresh Token
-  @Post('refresh')
+  
+  @Post('refresh')//v1.4.3- Refresh Token
   async refreshTokens(@Body() refreshTokenDto:RefreshTokenDto){
     return this.AuthService.refreshTokens(refreshTokenDto.refreshToken);
   }
 
-  //v1.5.1- ForgetPassword
-  @Post('resetpass')
+  
+  @Post('resetpass')//v1.5.1- ForgetPassword
   async reserPassword(@Body() forgotPass:ForgotPassDto){
     return this.AuthService.forgotPass(forgotPass);
   }
+
   //v1.7.0 - Change Passward
   @UseGuards(AuthGuard) // Protect all routes
   @Put('change-pass')
@@ -53,6 +51,7 @@ export class AuthController {
     const userId = req.userId; // Assuming the userId is in the JWT payload
     return await this.AuthService.changePassword(userId, changePass);
   }
+  
   //v1.7.5- Logout
   @UseGuards(AuthGuard) // Protect all routes
   @Post('logout')
